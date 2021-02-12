48aa51ce3e9beb675333960d93487de4aca630ea
Ivo Raisr <i.raisr@rheagroup.com>
2021-02-12 04:51:04 +0100

    Use view binding extension

--- app/src/main/java/com/rheagroup/efcr/app/ui/message/DisplayMessageFragment.kt
+++ app/src/main/java/com/rheagroup/efcr/app/ui/message/DisplayMessageFragment.kt
@@ -8,17 +8,16 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
 import com.rheagroup.efcr.app.R
+import com.rheagroup.efcr.app.databinding.DisplayMessageBinding
 
 class DisplayMessageFragment : Fragment() {
     private val args: DisplayMessageFragmentArgs by navArgs()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.display_message, container, false)
+        val binding = DisplayMessageBinding.inflate(inflater, container, false)
 
-        view.findViewById<TextView>(R.id.textView).apply {
-            text = args.message
-        }
+        binding.textView.text = args.message
 
-        return view
+        return binding.root
     }
 }
--- app/src/main/java/com/rheagroup/efcr/app/ui/message/SendMessageFragment.kt
+++ app/src/main/java/com/rheagroup/efcr/app/ui/message/SendMessageFragment.kt
@@ -8,23 +8,23 @@ import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.rheagroup.efcr.app.R
+import com.rheagroup.efcr.app.databinding.SendMessageBinding
 
 class SendMessageFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.send_message, container, false)
+        val binding = SendMessageBinding.inflate(inflater, container, false)
 
-        view.findViewById<Button>(R.id.send_button).setOnClickListener {
-            sendMessage(view)
+        binding.sendButton.setOnClickListener {
+            sendMessage(binding.editText.text.toString())
         }
 
-        return view
+        return binding.root
     }
 
-    private fun sendMessage(view: View) {
-        val message = view.findViewById<EditText>(R.id.editText).text.toString()
-
+    private fun sendMessage(message: String) {
         val action = SendMessageFragmentDirections.actionSendMessage(message)
-        view.findNavController().navigate(action)
+        findNavController().navigate(action)
     }
 }
